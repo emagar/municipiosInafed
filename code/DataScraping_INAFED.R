210716ed4d7555c6a3c3c96a885afac89386d604
Eric Magar <emagar@gmail.com>
2018-10-17 15:13:32 -0500

    Revisa código scraping de Camila para q output se guarde con nombres correctos

--- code/DataScraping_INAFED.R
+++ code/DataScraping_INAFED.R
@@ -1,33 +1,51 @@
-dat.vot<- read.csv('~/Desktop/Magar/elecRetrns/data/aymu1977-present.csv', stringsAsFactors = FALSE, encoding = "UTF-8", local= TRUE)
+-*- coding: utf-8-unix;-*-
+
+pathCamila <- "~/Desktop/Magar/"                 
+pathEric <- "/home/eric/Desktop/MXelsCalendGovt/"
+# elegir uno u otro
+path <- pathEric
+
+dat.vot<- if (path==pathCamila){
+              read.csv(paste0(path, "elecRetrns/data/aymu1977-present.csv"), stringsAsFactors = FALSE, encoding = "UTF-8", local= TRUE)
+          } else {
+              read.csv(paste0(path, "elecReturns/data/aymu1977-present.csv"), stringsAsFactors = FALSE)
+          }
+
 View(dat.vot)
-edon<-1
-estado <- c("aguascalientes", "bajacalifornia", "bajacaliforniasur", "campeche","coahuila","colima","chiapas","chihuahua","DF","durango","guanajuato","guerrero","hidalgo","jalisco","mexico","michoacan","morelos","nayarit","nuevoleon","oaxaca","puebla","queretaro","quintanaroo", "sanluispotosi","sinaloa","sonora","tabasco","tamaulipas","tlaxcala","veracruz","yucatan","zacatecas")[edon]
+dat.vot[1,]
+
+# selecciona qué estado procesará el código
+edon <- 1
+estado <- c("aguascalientes", "bajacalifornia", "bajacaliforniasur", "campeche", "coahuila", "colima", "chiapas", "chihuahua", "DF", "durango", "guanajuato", "guerrero", "hidalgo", "jalisco", "mexico", "michoacan", "morelos", "nayarit", "nuevoleon", "oaxaca", "puebla", "queretaro", "quintanaroo", "sanluispotosi", "sinaloa", "sonora", "tabasco", "tamaulipas", "tlaxcala", "veracruz", "yucatan", "zacatecas")[edon]
 edo <- c("ags", "bc", "bcs", "cam", "coa", "col", "cps", "cua", "df", "dgo", "gua", "gue", "hgo", "jal", "mex", "mic", "mor", "nay", "nl", "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac")[edon]
 
-setwd(paste0("~/Desktop/Magar/municipiosInafed/codigoFuente/",edo)) # adapta a la ruta de tu máquina
+setwd(paste0(path, "municipiosInafed/codigoFuente/", edo)) # ruta para guardar los datos
 
-munn <- dat.vot$inegi[which(dat.vot$edon==edon)]
-mun <- dat.vot$mun[which(dat.vot$edon==edon)]
+tmp <- dat.vot[which(dat.vot$edon==edon), c("inegi", "mun")]
+tmp <- tmp[duplicated(tmp$inegi)==FALSE,]
+head(tmp)
 
-web<- if (edon<10){
-  paste0('http://www.inafed.gob.mx/work/enciclopedia/EMM0', edon, estado, "/municipios/0", munn, "a.html")
-} else {
-  paste0('http://www.inafed.gob.mx/work/enciclopedia/EMM', edon, estado, "/municipios/", munn, "a.html")
-}
+inegi <- tmp$inegi
+munn <- inegi - edon*1000
+mun  <- tmp$mun
+rm(tmp)
 
-webpaths<- unique(web)
+webpaths <- if (edon<10){
+           paste0('http://www.inafed.gob.mx/work/enciclopedia/EMM0', edon, estado, "/municipios/0", inegi, "a.html")
+       } else {
+           paste0('http://www.inafed.gob.mx/work/enciclopedia/EMM' , edon, estado, "/municipios/" , inegi, "a.html")
+       }
+
 webpaths
 length(webpaths)
-munn
 
 for(i in 1:length(webpaths)) {
-  url.temp<-file(webpaths[i], encoding = "UTF-8")
-  txt.temp<- readLines(url.temp, encoding = "UTF-8")
-  write.csv(txt.temp, (if (munn<1010){
-    paste0(edon,"00",i,".csv")
-  } else {
-    paste0(edon,"0",i,".csv")
-  }), row.names = FALSE)
-  close(url.temp)
+    filename <- as.character(edon*1000 + munn[i])
+    url.temp <- file(webpaths[i], encoding = "UTF-8")
+    txt.temp <-  readLines(url.temp, encoding = "UTF-8")
+    write.csv(txt.temp, paste0(filename, ".csv") , row.names = FALSE)
+    close(url.temp)
 }
+
+rm(filename, url.temp, txt.temp) # limpieza
 
