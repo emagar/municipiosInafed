a0f083272b67fe6e76cfd52eb34b4fd7614700f7
Camila Blanes <cam.blanes@gmail.com>
2018-10-17 13:15:58 -0500

    add files codigoFuente/AGS

--- code/DataScraping_INAFED.R
+++ code/DataScraping_INAFED.R
@@ -4,7 +4,7 @@ edon<-1
 estado <- c("aguascalientes", "bajacalifornia", "bajacaliforniasur", "campeche","coahuila","colima","chiapas","chihuahua","DF","durango","guanajuato","guerrero","hidalgo","jalisco","mexico","michoacan","morelos","nayarit","nuevoleon","oaxaca","puebla","queretaro","quintanaroo", "sanluispotosi","sinaloa","sonora","tabasco","tamaulipas","tlaxcala","veracruz","yucatan","zacatecas")[edon]
 edo <- c("ags", "bc", "bcs", "cam", "coa", "col", "cps", "cua", "df", "dgo", "gua", "gue", "hgo", "jal", "mex", "mic", "mor", "nay", "nl", "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac")[edon]
 
-setwd(paste0("municipiosInafed/codigoFuente/",edo)) # adapta a la ruta de tu máquina
+setwd(paste0("~/Desktop/Magar/municipiosInafed/codigoFuente/",edo)) # adapta a la ruta de tu máquina
 
 munn <- dat.vot$inegi[which(dat.vot$edon==edon)]
 mun <- dat.vot$mun[which(dat.vot$edon==edon)]
@@ -18,11 +18,16 @@ web<- if (edon<10){
 webpaths<- unique(web)
 webpaths
 length(webpaths)
+munn
 
 for(i in 1:length(webpaths)) {
   url.temp<-file(webpaths[i], encoding = "UTF-8")
   txt.temp<- readLines(url.temp, encoding = "UTF-8")
-  write.csv(txt.temp, paste0(estado," ",i), row.names = FALSE)
+  write.csv(txt.temp, (if (munn<1010){
+    paste0(edon,"00",i,".csv")
+  } else {
+    paste0(edon,"0",i,".csv")
+  }), row.names = FALSE)
   close(url.temp)
-  
 }
+
