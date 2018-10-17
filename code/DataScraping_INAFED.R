5c73dd97905f93787098cbbd7a294527d0f9211c
Eric Magar <emagar@gmail.com>
2018-10-17 00:34:27 -0500

    Añade estructura estatal a carpetas codigoFuente y alcaldes

--- code/DataScraping_INAFED.R
+++ code/DataScraping_INAFED.R
@@ -1,17 +1,18 @@
 dat.vot<- read.csv('~/Desktop/Magar/elecRetrns/data/aymu1977-present.csv', stringsAsFactors = FALSE, encoding = "UTF-8", local= TRUE)
 View(dat.vot)
 edon<-1
-edo <- c("aguascalientes", "bajacalifornia", "bajacaliforniasur", "campeche","coahuila","colima","chiapas","chihuahua","DF","durango","guanajuato","guerrero","hidalgo","jalisco","mexico","michoacan","morelos","nayarit","nuevoleon","oaxaca","puebla","queretaro","quintanaroo",
-         "sanluispotosi","sinaloa","sonora","tabasco","tamaulipas","tlaxcala","veracruz","yucatan","zacatecas")[edon]
+estado <- c("aguascalientes", "bajacalifornia", "bajacaliforniasur", "campeche","coahuila","colima","chiapas","chihuahua","DF","durango","guanajuato","guerrero","hidalgo","jalisco","mexico","michoacan","morelos","nayarit","nuevoleon","oaxaca","puebla","queretaro","quintanaroo", "sanluispotosi","sinaloa","sonora","tabasco","tamaulipas","tlaxcala","veracruz","yucatan","zacatecas")[edon]
+edo <- c("ags", "bc", "bcs", "cam", "coa", "col", "cps", "cua", "df", "dgo", "gua", "gue", "hgo", "jal", "mex", "mic", "mor", "nay", "nl", "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac")[edon]
 
+setwd(paste0("municipiosInafed/codigoFuente/",edo)) # adapta a la ruta de tu máquina
 
 munn <- dat.vot$inegi[which(dat.vot$edon==edon)]
 mun <- dat.vot$mun[which(dat.vot$edon==edon)]
 
 web<- if (edon<10){
-  paste0('http://www.inafed.gob.mx/work/enciclopedia/EMM0', edon, edo, "/municipios/0", munn, "a.html")
+  paste0('http://www.inafed.gob.mx/work/enciclopedia/EMM0', edon, estado, "/municipios/0", munn, "a.html")
 } else {
-  paste0('http://www.inafed.gob.mx/work/enciclopedia/EMM', edon, edo, "/municipios/", munn, "a.html")
+  paste0('http://www.inafed.gob.mx/work/enciclopedia/EMM', edon, estado, "/municipios/", munn, "a.html")
 }
 
 webpaths<- unique(web)
@@ -21,7 +22,7 @@ length(webpaths)
 for(i in 1:length(webpaths)) {
   url.temp<-file(webpaths[i], encoding = "UTF-8")
   txt.temp<- readLines(url.temp, encoding = "UTF-8")
-  write.csv(txt.temp, paste0(edo," ",i), row.names = FALSE)
+  write.csv(txt.temp, paste0(estado," ",i), row.names = FALSE)
   close(url.temp)
   
 }
